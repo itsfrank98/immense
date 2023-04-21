89dd6feac20e5b04c2845f2c2d54fa64c7539923
itsfrank98 <frybene@libero.it>
2023-04-21 18:44:49 +0200

    added example

---
+++ examples.sh
@@ -0,0 +1,6 @@
+# The following trains sairus using node2vec as technique for learning both spatial and relational node embeddings. Spatial node embeddings have size 64,
+# relational node embeddings have size 128. The other parameters are left to their default values
+python main_train.py --textual_content_path dataset/posts_labeled.csv --social_net_url node_classification/graph_embeddings/stuff/social_network.edg --spatial_net_url node_classification/graph_embeddings/stuff/spatial_network.edg --word_embedding_size 256 --w2v_epochs 3 --spat_technique node2vec --rel_technique node2vec --spat_node_embedding_size 128 --rel_node_embedding_size 64
+
+# Test the previously learned model
+python main_test.py --spat_technique node2vec --rel_technique node2vec --models_dir models --dataset_dir dataset --word_embedding_size 256
